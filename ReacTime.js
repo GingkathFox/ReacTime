178b0393b745d23892921a32c1e3058f4455228a
Gingka Akiyama <33764485+GingkathFox@users.noreply.github.com>
2018-04-10 02:43:24 -0400

    Copied and modified code from ChannelBot

--- ReacTime.js
+++ ReacTime.js
@@ -1,1 +1,33 @@
+const Discord = require("discord.js");
+const { token, channel } = require('./config.json');
+const client = new Discord.Client();
 
+client.on("ready", () => {
+  console.log("Ready to edit perms!");
+  client.channels.get("channel");
+});
+//change the numbers with the
+//channel you want by using \#channel-name in Discord
+client.on('message', message => {
+  let channel = message.channel;
+  let roles = message.guild.roles;
+  //if (remainTime == 0) {
+    // overwrites 'ADD_REACTIONS' role, only on this specific channel
+    channel.overwritePermissions(
+        testRole,
+        { 'ADD_REACTIONS': false },
+        // optional 'reason' for permission overwrite
+        'Timer over!'
+    )
+    // handle responses / errors
+    .then(console.log)
+    .catch(console.log);
+       }  //function closing
+    }  //if closing
+
+   if (message.content === '${prefix}ping') {
+        message.channel.send('Pong.');
+    }
+});
+
+client.login(token);
