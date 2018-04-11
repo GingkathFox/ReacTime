f78433285541df01a2325f6fe1dec63fdafc049e
Gingka Akiyama <33764485+GingkathFox@users.noreply.github.com>
2018-04-10 22:05:47 -0400

    Changed `let` for role to be edited

--- ReacTime.js
+++ ReacTime.js
@@ -13,12 +13,12 @@ client.on('message', message => {
   let channel = message.channel;
   let roles = message.guild.roles;
   // find specific role - enter name of a role you create here
-    let testRole = roles.find('name', '<name of role>');
+    let reactRole = roles.find('name', '<name of role>');
 
   //if (remainTime == 0) {
     // overwrites 'ADD_REACTIONS' role, only on this specific channel
     channel.overwritePermissions(
-       // <name of role> 
+           reactRole
          'ADD_REACTIONS': false },
         // optional 'reason' for permission overwrite
         'Timer over!'
