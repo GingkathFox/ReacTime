7d1413589ac5b869b8256cf6dfdc26ffc4dea6ab
Gingka Akiyama <33764485+GingkathFox@users.noreply.github.com>
2018-04-13 22:46:27 -0400

    Update ReacTime.js

--- ReacTime.js
+++ ReacTime.js
@@ -11,23 +11,8 @@ client.on("ready", () => {
 //channel you want by using \#channel-name in Discord
 client.on('message', message => {
   let channel = message.channel;
-  let roles = message.guild.roles;
-  // find specific role - enter name of a role you create here
-    let reactRole = roles.find('name', '<name of role>');
-
-  //if (remainTime == 0) {
-    // overwrites 'ADD_REACTIONS' role, only on this specific channel
-    channel.overwritePermissions(
-           reactRole
-         'ADD_REACTIONS': false },
-        // optional 'reason' for permission overwrite
-        'Timer over!'
-    )
-    // handle responses / errors
-    .then(console.log)
-    .catch(console.log);
-       }  //function closing
-    }  //if closing
+  
+  setTimeout(() => message.channel.send("@everyone /nThe poll is over!), parsedTime)
 
    if (message.content === '${prefix}ping') {
         message.channel.send('Pong.');
